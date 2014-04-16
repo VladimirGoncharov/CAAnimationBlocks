becaa10bf9c271dca9ce0af3479b9f4e41d309d2
Goncharov Vladimir <vladimir.goncharov@flatstack.com>
2014-04-17 01:10:18 +0400

    add animations

--- Classes/CAAnimation+Blocks.h
+++ Classes/CAAnimation+Blocks.h
@@ -7,3 +7,17 @@
 @property (nonatomic, copy) void (^completion)(BOOL finished);
 
 @end
+
+//completed animated
+
+extern NSString *const UIViewAnimationTypeBounce;
+extern NSString *const UIViewAnimationTypeWiggle;
+
+@interface CALayer (ANIMATIONS)
+
+- (BOOL)animationEnabled:(NSString *)animation;
+- (void)animationPerform:(NSString *)animation
+              completion:(void(^)(BOOL))completion;
+- (void)animationCancel:(NSString *)animation;
+
+@end
--- Classes/CAAnimation+Blocks.m
+++ Classes/CAAnimation+Blocks.m
@@ -82,3 +82,122 @@
 }
 
 @end
+
+
+NSString *const UIViewAnimationTypeBounce       = @"__UIViewAnimationTypeBounce__";
+NSString *const UIViewAnimationTypeWiggle       = @"__UIViewAnimationTypeWiggle__";
+
+@implementation CALayer (ANIMATIONS)
+
+- (BOOL)animationEnabled:(NSString *)animation
+{
+    return ([self animationForKey:animation] != nil);
+}
+
+- (void)animationPerform:(NSString *)animation
+              completion:(void(^)(BOOL))completion
+{
+    if ([self animationEnabled:animation])
+    {
+#if DEBUG
+        NSLog(@"%s [warning] - animation(%@) is enabled. ignoring...", __func__, animation);
+#endif
+        if (completion)
+        {
+            completion(NO);
+        }
+        
+        return;
+    }
+    
+    CAAnimation *performAnimation       = nil;
+    performAnimation                    = [self _makeAnimationForKey:animation
+                                                          completion:completion];
+    if (performAnimation)
+    {
+        __unsafe_unretained typeof(self) wself                               = self;
+        void(^blockAction)() =
+        ^{
+            [wself addAnimation:performAnimation
+                         forKey:animation];
+        };
+        
+        if ([NSThread isMainThread])
+        {
+            blockAction();
+        }
+        else
+        {
+            dispatch_sync(dispatch_get_main_queue(), blockAction);
+        }
+    }
+    else
+    {
+        if (completion)
+        {
+#if DEBUG
+            NSLog(@"%s [warning] - animation(%@) not found. ignoring...", __func__, animation);
+#endif
+            completion(NO);
+        }
+    }
+}
+
+- (void)animationCancel:(NSString *)animation
+{
+    [self removeAnimationForKey:animation];
+}
+
+#pragma mark - private
+
+- (CAAnimation *)_makeAnimationForKey:(NSString *)key
+                           completion:(void(^)(BOOL))completion
+{
+    __unsafe_unretained typeof(self) wself                               = self;
+    void(^endAnimation)(BOOL) =
+    ^(BOOL finished){
+        if (!wself)
+            return;
+        
+        [wself animationCancel:key];
+        if (completion)
+        {
+            completion(finished);
+        }
+    };
+    
+    CAAnimation *animation          = nil;
+    if ([key isEqualToString:UIViewAnimationTypeBounce])
+    {
+        CAKeyframeAnimation *bounceAnimation   = [CAKeyframeAnimation animationWithKeyPath:@"transform.scale.xy"];
+        bounceAnimation.values = @[@1.0, @1.05, @0.95, @1.02, @0.98, @1.0];
+        [bounceAnimation setTimingFunctions:@[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                              [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                              [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                              [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                              [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]]];
+        bounceAnimation.duration                = 1.0f;
+        [bounceAnimation setCompletion:endAnimation];
+        
+        animation                               = bounceAnimation;
+    }
+    else if ([key isEqualToString:UIViewAnimationTypeWiggle])
+    {
+        CAKeyframeAnimation *rotationAnimation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"];
+        rotationAnimation.values = @[@0, @(M_PI / 180 * 5), @(M_PI / 180 * -5), @(M_PI / 180 * 2), @(M_PI / 180 * -2), @0];
+        [rotationAnimation setTimingFunctions:@[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                                [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                                [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                                [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                                                [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]]];
+        rotationAnimation.duration = 1.0f;
+        
+        [rotationAnimation setCompletion:endAnimation];
+        
+        animation                               = rotationAnimation;
+    }
+    
+    return animation;
+}
+
+@end
